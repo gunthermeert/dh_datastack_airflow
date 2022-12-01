4055b5126ac350fab15054c87f8a588cb974041a
GuntherMeert <gunthermeert@hotmail.com>
2022-12-01 16:09:03 +0100

    test pythonoperator

--- dbt_dag_builder.py
+++ dbt_dag_builder.py
@@ -57,6 +57,7 @@ with DAG(
         dbt_project_dir=DBT_PROJECT_DIR,
         dbt_profiles_dir=DBT_PROFILES_DIR,
         dbt_target=DBT_TARGET,
+        dbt_model_run="stg_dh_shop__customers"
     )
 
     dbt_run_group = dag_parser.get_dbt_run_group()
