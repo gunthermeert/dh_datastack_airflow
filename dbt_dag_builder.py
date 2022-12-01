c9196086feb49280bad6981b1bde152b51b0a07b
GuntherMeert <gunthermeert@hotmail.com>
2022-12-01 15:01:52 +0100

    test pythonoperator

--- dbt_dag_builder.py
+++ dbt_dag_builder.py
@@ -65,7 +65,7 @@ with DAG(
     @task(task_id="print_the_context")
     def print_context(ds=None, **kwargs):
         """Print the Airflow context and ds variable from the context."""
-        key = {{dag.params["model_run"].keys()}}
+        key = {{dag.params["model_run"]}}
         return key
 
     run_this = print_context()
