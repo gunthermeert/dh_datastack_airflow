46e8724280a5e289dfa63385bf8f1e9ed6b91e7d
GuntherMeert <gunthermeert@hotmail.com>
2022-12-02 08:28:13 +0100

    test bash_sleep_1.py

--- bash_sleep_1.py
+++ bash_sleep_1.py
@@ -50,7 +50,7 @@ with DAG(
         dbt_project_dir=DBT_PROJECT_DIR,
         dbt_profiles_dir=DBT_PROFILES_DIR,
         dbt_target=DBT_TARGET,
-        dbt_model_run=str(Param["model_run"])#os.getenv('DBT_RUN_MODEL')#"stg_dh_shop__customers"
+        dbt_model_run="""{{params.model_run}}"""#os.getenv('DBT_RUN_MODEL')#"stg_dh_shop__customers"
     )
 
     dbt_run_group = dag_parser.get_dbt_run_group()
