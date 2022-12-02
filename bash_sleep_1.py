46d077cf009c1878b95c4099f18db975debe4e38
GuntherMeert <gunthermeert@hotmail.com>
2022-12-02 08:35:21 +0100

    test bash_sleep_1.py

--- bash_sleep_1.py
+++ bash_sleep_1.py
@@ -33,6 +33,7 @@ with DAG(
     start_dummy = DummyOperator(task_id="start")
 
     model_run = '{{params.model_run}}'
+    owner = "{{ dag_run.conf['owner'] }}"
 
     # test all sources
     t2 = BashOperator(
@@ -50,7 +51,7 @@ with DAG(
         dbt_project_dir=DBT_PROJECT_DIR,
         dbt_profiles_dir=DBT_PROFILES_DIR,
         dbt_target=DBT_TARGET,
-        dbt_model_run={"model_run": model_run},#os.getenv('DBT_RUN_MODEL')#"stg_dh_shop__customers"
+        dbt_model_run={"owner": owner},#os.getenv('DBT_RUN_MODEL')#"stg_dh_shop__customers"
     )
 
     dbt_run_group = dag_parser.get_dbt_run_group()
