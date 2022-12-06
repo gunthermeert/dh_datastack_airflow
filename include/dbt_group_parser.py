2f547b49aabfa678c30dcde3969f741b2ed2e3c7
GuntherMeert <gunthermeert@hotmail.com>
2022-12-06 14:15:26 +0100

    test adding source freshness

--- include/dbt_group_parser.py
+++ include/dbt_group_parser.py
@@ -146,7 +146,7 @@ class DbtDagParser:
 
         if len(freshness_dependency) > 0:
             source_freshness = freshness_dependency.split(".")[-2] + freshness_dependency.split(".")[-1] #we only want the source + modelname
-            FRESHNESS_COMMAND == f"""dbt source freshness --select source:{source_freshness}"""
+            FRESHNESS_COMMAND == f"""dbt source freshness --select source:{source_freshness} &&"""
         else:
             FRESHNESS_COMMAND == ""
 
@@ -156,7 +156,7 @@ class DbtDagParser:
             task_group=self.dbt_run_group,
             bash_command=f"""
             cd {self.dbt_project_dir} &&
-            {FRESHNESS_COMMAND} &&
+            {FRESHNESS_COMMAND} 
             dbt {self.dbt_global_cli_flags} {DBT_COMMAND} --target dev --select {node_name} &&
             dbt {self.dbt_global_cli_flags} test --target dev --select {node_name}
             """,
