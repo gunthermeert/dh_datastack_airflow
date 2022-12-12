f9c825d32aee304062a932ec357201dcb7f605fc
GuntherMeert <gunthermeert@hotmail.com>
2022-12-12 09:21:15 +0100

    dag cleanup

--- include/dbt_dag_parser.py
+++ include/dbt_dag_parser.py
@@ -27,9 +27,10 @@ class DbtDagParser:
         dbt_project_dir=None,
         dbt_profiles_dir=None,
         dbt_target=None,
+        dbt_manifest_filepath=None,
         dbt_tag=None,
         dbt_run_group_name="dbt_run",
-        dbt_test_group_name="dbt_test",
+        dbt_model_run=None,
     ):
 
         self.dag = dag
@@ -37,95 +38,159 @@ class DbtDagParser:
         self.dbt_project_dir = dbt_project_dir
         self.dbt_profiles_dir = dbt_profiles_dir
         self.dbt_target = dbt_target
+        self.dbt_manifest_filepath = dbt_manifest_filepath
         self.dbt_tag = dbt_tag
 
         self.dbt_run_group = TaskGroup(dbt_run_group_name)
-        self.dbt_test_group = TaskGroup(dbt_test_group_name)
+        self.dbt_model_run = dbt_model_run
+
+        # load manifest file to compile airflow code
+        self.data = self.load_manifest()
+        # print(json.dumps(data, indent=1))
+
+        self.parent_map_data = self.parent_mapping_data()
+
+        # add model/snapshot node info to dbt_nodes
+        self.dbt_nodes = {}
+
 
         # Parse the manifest and populate the two task groups
         self.make_dbt_task_groups()
 
-    def load_dbt_manifest(self):
-        """
-        Helper function to load the dbt manifest file.
+    def load_manifest(self):
+        local_filepath = self.dbt_manifest_filepath #"_marketing/target/manifest.json" #"C:/Users/GuntherMeert/Downloads/manifest.json"
+        with open(local_filepath) as f:
+            data = json.load(f)
 
-        Returns: A JSON object containing the dbt manifest content.
+        return data
 
-        """
-        manifest_path = os.path.join(self.dbt_project_dir, "target/manifest.json")
-        with open(manifest_path) as f:
-            file_content = json.load(f)
-        return file_content
+    def parent_mapping_data(self):
+        data = self.load_manifest()
+        return data["parent_map"]
 
-    def make_dbt_task(self, node_name, dbt_verb):
-        """
-        Takes the manifest JSON content and returns a BashOperator task
-        to run a dbt command.
+    # check if source tables have freshness on them
+    def source_freshness_nodes(self):
+        source_freshness_nodes = []
+        data = self.load_manifest()
 
-        Args:
-            node_name: The name of the node
-            dbt_verb: 'run' or 'test'
+        for node in data["sources"].keys():
+            if node.split(".")[0] == "source": #check that's possibly not needed
+                if data["sources"][node]["freshness"]['error_after']['count'] != None: #we only want to keep sources that have a freshness defined on error
+                    source_freshness_nodes.append(node)
 
-        Returns: A BashOperator task that runs the respective dbt command
+        return source_freshness_nodes #list that will be checked when comparing node dependencies
 
-        """
+    def generate_all_nodes(self):
+        for node in self.data["nodes"].keys():
+            if node.split(".")[0] == "model" or node.split(".")[0] == "snapshot" or node.split(".")[0] == "seed":
+                self.dbt_nodes[node] = {}
 
-        model_name = node_name.split(".")[-1]
-        if dbt_verb == "test":
-            node_name = node_name.replace("model", "test")  # Just a cosmetic renaming of the task
-            task_group = self.dbt_test_group
+                self.dbt_nodes[node]['node_name'] = node.split(".")[-1]
+                self.dbt_nodes[node]['node_resource_type'] = self.data["nodes"][node]["resource_type"]
+
+                # dependencies: the manifest file might generate duplicate dependencies within the same node
+                # therefor we create a list with the distinct values
+                node_dependencies = [x for x in self.data["nodes"][node]["depends_on"]["nodes"] if
+                                     "source." not in x]  # this removes dbt source dependencies of the original list data["nodes"][node]["depends_on"]["nodes"]
+                node_dependencies_distinct = list(dict.fromkeys(node_dependencies))
+
+                self.dbt_nodes[node]['node_depends_on'] = node_dependencies_distinct
+
+                # check if we need to implement a source_freshness_check
+                list_source_freshness_nodes = self.source_freshness_nodes()
+                node_freshness_dependencies = [x for x in self.data["nodes"][node]["depends_on"]["nodes"] if "source." in x]
+
+                self.dbt_nodes[node]['freshness_dependency'] = ""
+
+                for node_freshness_dependency in node_freshness_dependencies:
+                    if node_freshness_dependency in list_source_freshness_nodes:
+                        self.dbt_nodes[node]['freshness_dependency'] = node_freshness_dependency
+
+    def iterate_parent_nodes(self, node):
+        # iterate over every node it's dependencies to see if that node has more depencies, so we can build parents from parents
+        if node.split(".")[0] == "model" or node.split(".")[0] == "snapshot" or node.split(".")[0] == "seed":
+            self.dbt_nodes[node] = {}
+
+            self.dbt_nodes[node]['node_name'] = node.split(".")[-1]
+            self.dbt_nodes[node]['node_resource_type'] = node.split(".")[0]
+            node_dependencies = [x for x in self.parent_map_data[node] if
+                                 "source." not in x]  # this removes dbt source dependencies of the original list data["nodes"][node]["depends_on"]["nodes"]
+            node_dependencies_distinct = list(dict.fromkeys(node_dependencies))
+
+            self.dbt_nodes[node]['node_depends_on'] = node_dependencies_distinct
+
+            # check if we need to implement a source_freshness_check
+            list_source_freshness_nodes = self.source_freshness_nodes()
+            node_freshness_dependencies = [x for x in self.data["nodes"][node]["depends_on"]["nodes"] if "source." in x]
+
+            self.dbt_nodes[node]['freshness_dependency'] = ""
+
+            for node_freshness_dependency in node_freshness_dependencies:
+                if node_freshness_dependency in list_source_freshness_nodes:
+                    self.dbt_nodes[node]['freshness_dependency'] = node_freshness_dependency
+
+        for parent_node in self.parent_map_data[node]:
+            self.iterate_parent_nodes(parent_node)
+
+    def make_dbt_task(self, node_name, node_resource_type, freshness_dependency):
+        """Returns an Airflow operator"""
+
+        if node_resource_type == "model":
+            DBT_COMMAND = "run"
+        if node_resource_type == "snapshot":
+            DBT_COMMAND = "snapshot"
+        if node_resource_type == "seed":
+            DBT_COMMAND = "seed"
+
+        if len(freshness_dependency) > 0:
+            source_freshness = freshness_dependency.split(".")[-2] + '.' + freshness_dependency.split(".")[-1] #we only want the source + modelname
+            FRESHNESS_COMMAND = f"""dbt source freshness --select source:{source_freshness} &&"""
         else:
-            task_group = self.dbt_run_group
+            FRESHNESS_COMMAND = ""
+
+
 
         dbt_task = BashOperator(
             task_id=node_name,
-            task_group=task_group,
-            bash_command=(
-                f"dbt {self.dbt_global_cli_flags} {dbt_verb} "
-                f"--target {self.dbt_target} --models {model_name} "
-                f"--profiles-dir {self.dbt_profiles_dir} --project-dir {self.dbt_project_dir}"
-            ),
+            task_group=self.dbt_run_group,
+            bash_command=f"""
+            cd {self.dbt_project_dir} &&
+            {FRESHNESS_COMMAND if FRESHNESS_COMMAND is not None else ""}
+            dbt {self.dbt_global_cli_flags} {DBT_COMMAND} --target dev --select {node_name} &&
+            dbt {self.dbt_global_cli_flags} test --target dev --select {node_name}
+            """,
             dag=self.dag,
         )
-        # Keeping the log output, it's convenient to see when testing the python code outside of Airflow
-        logging.info("Created task: %s", node_name)
+
         return dbt_task
 
+
     def make_dbt_task_groups(self):
-        """
-        Parse out a JSON file and populates the task groups with dbt tasks
 
-        Returns: None
+        # check if parameter model_run <> all
+        if self.dbt_model_run == 'all':
+            self.generate_all_nodes()
+        else:
+            if self.dbt_model_run in self.parent_map_data:
+                self.iterate_parent_nodes(self.dbt_model_run)
 
-        """
-        manifest_json = self.load_dbt_manifest()
-        dbt_tasks = {}
+        #create a bashoperator per dbt node, this must be done before creating the airflow order dependency
+        airflow_operators = {}
 
-        # Create the tasks for each model
-        for node_name in manifest_json["nodes"].keys():
-            if node_name.split(".")[0] == "model":
-                tags = manifest_json["nodes"][node_name]["tags"]
-                # Only use nodes with the right tag, if tag is specified
-                if (self.dbt_tag and self.dbt_tag in tags) or not self.dbt_tag:
-                    # Make the run nodes
-                    dbt_tasks[node_name] = self.make_dbt_task(node_name, "run")
+        for node in self.dbt_nodes:
+            airflow_operators[node] = self.make_dbt_task(self.dbt_nodes[node]["node_name"], self.dbt_nodes[node]["node_resource_type"], self.dbt_nodes[node]["freshness_dependency"])
 
-                    # Make the test nodes
-                    node_test = node_name.replace("model", "test")
-                    dbt_tasks[node_test] = self.make_dbt_task(node_name, "test")
+        #after creating the bash operators we must determine the scheduling order of the operators
+        for node in self.dbt_nodes:
 
+                node_dependencies = self.dbt_nodes[node]["node_depends_on"]
 
-        # Add upstream and downstream dependencies for each run task
-        for node_name in manifest_json["nodes"].keys():
-            if node_name.split(".")[0] == "model":
-                tags = manifest_json["nodes"][node_name]["tags"]
-                # Only use nodes with the right tag, if tag is specified
-                if (self.dbt_tag and self.dbt_tag in tags) or not self.dbt_tag:
-                    for upstream_node in manifest_json["nodes"][node_name]["depends_on"]["nodes"]:
-                        upstream_node_type = upstream_node.split(".")[0]
-                        if upstream_node_type == "model":
-                            dbt_tasks[upstream_node] >> dbt_tasks[node_name]
-
+                # check if there are node dependancies, because nodes without dependencies could exist
+                if(len(node_dependencies) == 0):
+                    airflow_operators[node]
+                else:
+                    for dependency in node_dependencies:
+                        airflow_operators[dependency] >> airflow_operators[node]
 
     def get_dbt_run_group(self):
         """
@@ -135,13 +200,4 @@ class DbtDagParser:
 
         """
         return self.dbt_run_group
-
-    def get_dbt_test_group(self):
-        """
-        Getter method to retrieve the previously constructed dbt tasks.
-
-        Returns: An Airflow task group with dbt test nodes.
-
-        """
-        return self.dbt_test_group
 
--- read_manifest.py
+++ read_manifest.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import datetime
-from include.dbt_group_parser import DbtDagParser
+from include.dbt_dag_parser import DbtDagParser
 
 # We're hardcoding these values here for the purpose of the demo, but in a production environment these
 # would probably come from a config file and/or environment variables!
