f6a931d85a810b51374319b2f52245317c5528a8
GuntherMeert <gunthermeert@hotmail.com>
2023-01-03 11:49:27 +0100

    create test dag for freshness

--- test_dh_customer_freshness.py
+++ test_dh_customer_freshness.py
@@ -8,6 +8,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import datetime
 from include.dbt_dag_parser import DbtDagParser
 from airflow.decorators import dag, task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 # We're hardcoding these values here for the purpose of the demo, but in a production environment these
 # would probably come from a config file and/or environment variables!
@@ -28,9 +29,9 @@ with DAG(
 ) as dag:
     start_dummy = DummyOperator(task_id="start")
 
-    # test all sources
+    # freshness check
     dbt_customer_freshness_check = BashOperator(
-        task_id="dbt_source_test",
+        task_id="dbt_customer_freshness_check",
         bash_command=
             f"""
             cd {DBT_PROJECT_DIR} &&
@@ -39,6 +40,29 @@ with DAG(
             dag=dag,
     )
 
+    # refresh customer
+    trigger_customer_refresh = TriggerDagRunOperator(
+        task_id='trigger_customer_refresh',
+        trigger_dag_id='dbt_dh_customer_freshness',
+        wait_for_completion=True,
+        trigger_rule='all_failed',
+        dag=dag
+    )
+
+    # run model
+    dbt_marketing_run_customer = BashOperator(
+        task_id="dbt_marketing_run_customer",
+        trigger_rule='one_success',
+        bash_command=
+            f"""
+            cd {DBT_PROJECT_DIR} &&
+            dbt --no-write-json run --target dev --select stg_dh_datastack_mdm__customers &&
+            dbt --no-write-json test --target dev --select stg_dh_datastack_mdm__customers
+            """,
+            dag=dag,
+    )
+
     end_dummy = DummyOperator(task_id="end")
 
-    start_dummy >> dbt_customer_freshness_check >> end_dummy
+    start_dummy >> dbt_customer_freshness_check >> dbt_marketing_run_customer >> end_dummy
+    start_dummy >> dbt_customer_freshness_check >> trigger_customer_refresh >> dbt_marketing_run_customer >> end_dummy
