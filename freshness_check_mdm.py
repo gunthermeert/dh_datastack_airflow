655ea95b35fffeb364e89a5a5d60242e62689124
GuntherMeert <gunthermeert@hotmail.com>
2023-01-04 16:12:33 +0100

    add parameter freshness_check_mdm

--- freshness_check_mdm.py
+++ freshness_check_mdm.py
@@ -3,6 +3,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import datetime
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.models.param import Param
 
 
 with DAG(
@@ -10,21 +11,25 @@ with DAG(
     start_date=datetime(2022, 11, 7),
     description='dbt dag that builds an airflow dag dynamically by reading manifest',
     schedule_interval="0 10 * * *",
+    params={
+            "freshness_hours": Param("1", type="string"),
+    },
     max_active_runs=1,
     catchup=False
 ) as dag:
     start_dummy = DummyOperator(task_id="start")
 
-
+    # number "1" and text "CUSTOMERS" should be variables
     freshness_check = BashOperator(
         task_id="freshness_check",
         bash_command=f"""
         cd /home/gunther/dh_datastack_dbt/dh_datastack_mdm &&
-        dbt source freshness --select source:mdm_freshness_1_hour.CUSTOMERS
+        dbt source freshness --select source:mdm_freshness_{{ params.freshness_hours }}_hour.CUSTOMERS 
         """,
         dag=dag,
     )
 
+    # customers should be a variable
     refresh_trigger = TriggerDagRunOperator(
         task_id="refresh_trigger",
         trigger_dag_id="refresh_dh_datastack_mdm_customers",
@@ -33,11 +38,12 @@ with DAG(
         dag=dag,
     )
 
+    # number "1" and text "CUSTOMERS" should be variables
     freshness_check_validation = BashOperator(
         task_id="freshness_check_validation",
         bash_command=f"""
         cd /home/gunther/dh_datastack_dbt/dh_datastack_mdm &&
-        dbt source freshness --select source:mdm_freshness_1_hour.CUSTOMERS
+        dbt source freshness --select source:mdm_freshness_{{ params.freshness_hours }}_hour.CUSTOMERS
         """,
         dag=dag,
     )
