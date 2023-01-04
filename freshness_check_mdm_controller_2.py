73e9245ede6753578763d3e97c5439ef95a4195a
GuntherMeert <gunthermeert@hotmail.com>
2023-01-04 17:21:53 +0100

    add parameter freshness_check_mdm

--- freshness_check_mdm_controller_2.py
+++ freshness_check_mdm_controller_2.py
@@ -9,7 +9,7 @@ from airflow.models.param import Param
 
 
 with DAG(
-    dag_id='freshness_check_mdm_controller',
+    dag_id='freshness_check_mdm_controller_2',
     start_date=datetime(2022, 11, 7),
     description='dbt dag that builds an airflow dag dynamically by reading manifest',
     schedule_interval="0 10 * * *",
